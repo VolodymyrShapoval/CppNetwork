4341414d159c9e69da3bc237b64facc9c402e448
VolodymyrS <volodymyr.sh.05@gmail.com>
2025-05-26 03:02:54 +0300

    add routing

--- HTTP_Server/src/HTTP_Server.h
+++ HTTP_Server/src/HTTP_Server.h
@@ -2,10 +2,13 @@
 #include <string>
 #include "IncludeFile.h"
 #include "Response.h"
+#include "Request.h"
 #include <iostream>
 #include <thread>
 #include <regex>
 #include <sstream>
+#include <functional>
+#include <map>
 
 namespace CNet
 {
@@ -32,11 +35,11 @@ namespace CNet
 			Network::shutdown(); // Clean up Winsock
 		}
 
-        // Set the public directory path
-        inline void publicDir(const std::string& dir) 
-        {
-            publicDirPath = dir;
-        }
+        // // Set the public directory path
+        // inline void publicDir(const std::string& dir) 
+        // {
+        //     publicDirPath = dir;
+        // }
 
         // Start the server on the specified port
         PResult start(int port) 
@@ -48,7 +51,7 @@ namespace CNet
 				return PResult::P_GENERIC_ERROR;
 			}
             
-			if (listen_socket.listen(IPEndpoint("192.168.0.107", port)) != PResult::P_SUCCESS) {
+			if (listen_socket.listen(IPEndpoint("10.211.55.4", port)) != PResult::P_SUCCESS) {
 				std::cerr << "Failed to listen on port " << port << std::endl;
 				int error = WSAGetLastError();
                 listen_socket.close();
@@ -73,26 +76,26 @@ namespace CNet
         }
 
         // Add a new route with a GET method
-        /*void get(const std::string& path, std::function<void(Request&, Response&)> callback) {
+        void get(const std::string& path, std::function<void(Request&, Response&)> callback) {
             assignHandler("GET", path, callback);
-        }*/
+        }
 
         // Add a new route with a POST method
-        /*void post(const std::string& path, std::function<void(Request&, Response&)> callback) {
+        void post(const std::string& path, std::function<void(Request&, Response&)> callback) {
             assignHandler("POST", path, callback);
-        }*/
+        }
 
     private:
         // Map to store the routes
-        //std::map<std::string, std::map<std::string, std::pair<std::string, std::function<void(Request&, Response&)>>>> routes;
+        std::map<std::string, std::map<std::string, std::pair<std::string, std::function<void(Request&, Response&)>>>> routes;
         // Path to the public directory
-        std::string publicDirPath;
+        std::string publicDirPath = "public";
 
         // Add event handler to the routes map
-       /* void assignHandler(const std::string& method, const std::string& path, std::function<void(Request&, Response&)> callback) {
+        void assignHandler(const std::string& method, const std::string& path, std::function<void(Request&, Response&)> callback) {
             std::string newPath = std::regex_replace(path, std::regex("/:\\w+/?"), "/([^/]+)/?");
             routes[method][newPath] = std::pair<std::string, std::function<void(Request&, Response&)>>(path, callback);
-        }*/
+        }
 
         // Handle the client request
         void handleClient(TCPSocket client_socket) 
@@ -105,103 +108,89 @@ namespace CNet
 			std::cout << "Request content:\n" << request << std::endl;
 
             Response response;
-            //Request req;
+            Request req;
 
-            /*if (path.find('?') != std::string::npos) 
+            std::smatch match;
+            for (auto& route : routes[method]) 
             {
-                std::string query_string = path.substr(path.find('?') + 1);
-                path = path.substr(0, path.find('?'));
-                std::istringstream query_iss(query_string);
-                std::string query_pair;
-                while (std::getline(query_iss, query_pair, '&')) {
-                    std::string key = query_pair.substr(0, query_pair.find('='));
-                    std::string value = query_pair.substr(query_pair.find('=') + 1);
-                    req.query[key] = value;
-                }
-            }*/
-
-
-            /*std::smatch match;
-            for (auto& route : routes[method]) {
                 std::string route_path = route.first;
 
-                if (std::regex_match(path, match, std::regex(route_path))) {
+                if (std::regex_match(path, match, std::regex(route_path))) 
+                {
                     std::regex token_regex(":\\w+");
                     std::string originalPath = routes[method][route_path].first;
 
                     std::vector<std::string> tokens = split_(originalPath, '/');
-                    while (std::regex_search(originalPath, match, token_regex)) {
+                    while (std::regex_search(originalPath, match, token_regex)) 
+                    {
                         const std::string match_token = match.str();
                         int position = 0;
-                        for (int i = 0; i < tokens.size(); i++) {
-                            if (tokens[i] == match_token) {
+                        for (int i = 0; i < tokens.size(); i++) 
+                        {
+                            if (tokens[i] == match_token) 
+                            {
                                 position = i;
                                 break;
                             }
                         }
-
-                        std::vector<std::string> path_tokens = split_(path, '/');
-                        req.params[match_token.substr(1)] = path_tokens[position];
-
                         originalPath = match.suffix();
                     }
 
                     routes[method][route_path].second(req, response);
-                    serve_static_file(path, response);
-                    send_response(client_socket, response);
-                    SOCKET_CLOSE(client_socket);
+                    serveStaticFile(path, response);
+                    sendResponse(client_socket, response);
+                    client_socket.close();
                     return;
                 }
-            }*/
+            }
 
-            //// Serve static files if not matched by any route
-            //serve_static_file(path, response);
-            send_response(client_socket, response);
+            // Serve static files if not matched by any route
+            serveStaticFile(path, response);
+            sendResponse(client_socket, response);
 			client_socket.close(); // Close the client socket after handling the request
         }
 
-        //void serve_static_file(const std::string& path, Response& response) {
+        void serveStaticFile(const std::string& path, Response& response) 
+        {
+            std::string requestedPath = path;
 
-        //    // Determine the content type based on the file extension
-        //    std::string content_type;
-        //    std::string file_extension = path.substr(path.find_last_of('.') + 1);
-        //    if (file_extension == "html") {
-        //        content_type = "text/html";
-        //    }
-        //    else if (file_extension == "css") {
-        //        content_type = "text/css";
-        //    }
-        //    else if (file_extension == "js") {
-        //        content_type = "application/javascript";
-        //    }
-        //    else if (file_extension == "json") {
-        //        content_type = "application/json";
-        //    }
-        //    else if (file_extension == "jpg" || file_extension == "jpeg") {
-        //        content_type = "image/jpeg";
-        //    }
-        //    else if (file_extension == "png") {
-        //        content_type = "image/png";
-        //    }
-        //    else if (file_extension == "gif") {
-        //        content_type = "image/gif";
-        //    }
-        //    else {
-        //        // Default to octet-stream for unknown file types
-        //        content_type = "application/octet-stream";
-        //    }
-        //    std::fstream file(publicDirPath + path, std::ios::in | std::ios::binary);
-        //    if (file) {
+            if (requestedPath == "/") {
+                requestedPath = "/index.html";
+            }
 
-        //        response << std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        //        file.close();
-        //        // Set the response headers
-        //        response.header("Content-Type", content_type);
-        //    }
-        //}
+            std::string fullPath = publicDirPath + requestedPath;
+
+            std::fstream file(fullPath, std::ios::in | std::ios::binary);
+            if (file)
+            {
+                std::ostringstream buffer;
+                buffer << file.rdbuf();
+                file.close();
+
+                std::string content_type;
+                std::string file_extension = requestedPath.substr(requestedPath.find_last_of('.') + 1);
+                if (file_extension == "html") 
+                    content_type = "text/html";
+                else if (file_extension == "jpg" || file_extension == "jpeg")
+                    content_type = "image/jpeg";
+                else
+                    content_type = "application/octet-stream";
+
+                response.body = buffer.str();
+                response.header("Content-Type", content_type);
+                response.status = "200 OK";
+            }
+            else
+            {
+                response.status = "404 Not Found";
+                response.body = "<h1>404 Not Found</h1>";
+                response.header("Content-Type", "text/html");
+            }
+        }
 
         // Read the request from the client
-        std::string readRequest(TCPSocket client_socket) {
+        std::string readRequest(TCPSocket client_socket) 
+        {
             std::string request;
             char buffer[1024]; // Buffer to store the request
             int bytes_received; // Bytes received from the client
@@ -216,15 +205,14 @@ namespace CNet
             return request;
         }
 
-        void send_response(TCPSocket client_socket, Response response) {
+        void sendResponse(TCPSocket client_socket, Response response) 
+        {
             response.header("Content-Length", std::to_string(response.body.size()));
             response.header("X-Powered-By", "Xebec-Server/0.1.0");
             response.header("Programming-Language", "C++");
             response.headers += "\r\n";
             std::string res = "HTTP/1.1 " + response.status + response.headers + response.body;
-			int bytesSent = 0;
-			client_socket.sendAll(res.c_str(), res.size());
+            client_socket.sendAll(res.c_str(), res.size());
         }
-
     };
 }
